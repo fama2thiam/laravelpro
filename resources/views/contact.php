fb6933acc3d130eb0f084bb1925de1207851114d
THIAM <famativ2@gmail.com>
2017-07-14 17:52:12 +0000

    mettre en place le projet laravel sur github

---
+++ resources/views/contact.php
@@ -0,0 +1,27 @@
+
+
+<HTML>
+<HEAD>
+<TITLE> Inscription </TITLE>
+</HEAD>
+<BODY>
+<H1>Informations sur FAMA <BR></H1>
+<table> 
+<TR>
+
+<div align="center"> Prenoms: Fama </div><br>
+
+
+<div align="center">  nom :  THIAM </div><br>
+
+<div align="center"> Profession : Etudiante </div><br>
+
+<div align="center"> niveau : L2 </div><br>
+
+<div align="center"> addresse : HLM Grand Yoff </div><br>
+
+<div align="center"> Tel : 77 765 77 91</div><br>
+<img src="C:\Users\G50\Pictures\Mes photos\C360_2016-04-09-21-56-13-836.jpg" alt="" />
+
+</BODY>
+</HTML>
---
+++ resources/views/image.php
@@ -0,0 +1,9 @@
+<html>
+<Head> 
+ <Title> Etudiante en tdsi</Title>
+ </Head>
+ <Body BACKGROUND="C:\Users\G50\Pictures\Mes photos\butterfly.jpg" text="ffffff" link="rrvvbb">
+
+ </body>
+ 
+ </html>
